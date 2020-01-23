da1fd7bc1a45969b62832428c0db6f0adc5e58b8
Alex Wright <jawright94@gmail.com>
2020-01-23 15:05:35 -0500

    feat/application-model/identified the primary states of 'idle' and 'selected' as well as the necessary context of 'subreddit' and the transitiona to switch between the two states

--- redditMachine.js
+++ redditMachine.js
@@ -1,10 +1,27 @@
 import { Machine } from './xstate'
+import { assign } from 'xstate/lib/actionTypes'
+
+const selectEvent = {
+    type: 'SELECT', //event type
+    name: 'reactjs' //subreddit name
+}
 
 const redditMachine = Machine({
     id: 'reddit',
     initial: 'idle',
+    context: {
+        subreddit: null // no subreddit has been selected yet in the initial state
+    },
     states: {
         idle: {},
         selected: {}
+    },
+    on: {
+        SELECT: {
+            target: '.selected',
+            actions: assign({
+                subreddit: (context, event) => event.name
+            })
+        }
     }
 })
